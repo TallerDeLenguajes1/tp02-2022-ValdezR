91aa5f078e1c2429e763e3964e087f8f8269531a
ValdezR <valdez.ramonh@gmail.com>
2022-04-29 00:30:31 -0300

    primer commit punto a

---
+++ tp2_1_1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+#define N 20
+
+int main(){
+
+    int i;
+    double vt[N];
+
+    for(i = 0;i<N; i++)
+    {
+        vt[i]=1+rand()%100;
+        printf("%f", vt[i]);
+        printf("\n");
+    }
+    
+    return 0;
+}
